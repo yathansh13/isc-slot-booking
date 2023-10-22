359cac854e328f7ca7ef4de7c16a2d997b25b3da
yathansh13 <yathansh1312@gmail.com>
2023-10-22 19:35:48 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "../app/images/landing.svg";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>Hi </p>
+      
+      </div>
+    </main>
+  );
+}
